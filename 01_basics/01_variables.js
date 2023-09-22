d0c343d078af00d6ca80cc81872df792f098b040
Sunanda Chakraborty <109503742+sunanda073@users.noreply.github.com>
2023-09-22 07:40:59 +0000

    variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,4 +1,19 @@
 const accountId = 1445567;
 let accountEmail = "sunanda@gmail.com";
-var accountPassword = "012";
+var accountPassword = "012"; 
 accountCity = "Kolkata";
+let accountState; //undefined
+
+//accountId=2; // not allowed
+accountEmail= "apple@gmail.com";
+accountPassword= 2121;
+accountCity="Bangaluru";
+
+console.log(accountId);
+
+console.table([accountCity, accountId, accountPassword, accountEmail, accountState])
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
