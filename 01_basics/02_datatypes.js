c59cf1e46d32f66c60930540c5c5628c3f8d90b3
Sunanda Chakraborty <109503742+sunanda073@users.noreply.github.com>
2023-09-22 08:12:36 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all Js code as newer version
+
+// alert(3+8); // we are usinh nodejs, not browser
+
+console.log(3-9) //code readability should be high
+
+let name = "Sunanda"
+let age = 20
+let isLoggedIn = false
+
+// number => 2 to the power 53
+// bigint 
+// String => ""
+// Boolean => true / false
+// null (standalone value) => representation of empty value => it is one type of object
+// undefined 
+// symbol => unique
+
+// object
+
+console.log(typeof name);
+console.log(typeof null);
+console.log(typeof undefined);
