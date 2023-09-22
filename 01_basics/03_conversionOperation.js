9ac5a77c1fce0efb29a9904473bf5c75cfa3162c
Sunanda Chakraborty <109503742+sunanda073@users.noreply.github.com>
2023-09-22 13:04:57 +0000

    operations done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -17,10 +17,11 @@ undefined => NaN
 String (sunanda) => NaN
 */
 
-// let isLoggedIn = " "
-// let booleanIsLoggedIn = Boolean(isLoggedIn);
+//  let isLoggedIn = " "
+//  let booleanIsLoggedIn = Boolean(isLoggedIn);
 
-// console.log(booleanIsLoggedIn);
+//  console.log(booleanIsLoggedIn);
+//  console.log(typeof booleanIsLoggedIn);
 
 /*
 ""(empty string) => false
@@ -29,9 +30,47 @@ string ("sunanda") => true
 0 =>false
 */
 
-let someNumber = 33.30
-let stringNumber = String(someNumber)
+// let someNumber = 33.30
+// let stringNumber = String(someNumber)
 
-console.log(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+// ************************Operations**********************************
+
+let value =3
+let negValue = -value
+// console.log(negValue);
+// console.log(2**3);
+// console.log(2*3);
+// console.log(2+3);
+// console.log(3-1);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello"
+let str2 = " Sunanda"
+let str3 = str1 + str2
+// console.log(str3);
+// console.log("1"+2);
+// console.log(1 + "2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+// console.log(3+4*5%3);
+
+// console.log(true);
+// console.log(+true);
+// console.log(typeof +true);
+// console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+// console.table([num1, num2, num3]);
+
+let gameCounter = 1;
+gameCounter++;
+++gameCounter;
+console.log(gameCounter);
