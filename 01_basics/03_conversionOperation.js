5de850a3fdbf0b47a53ebc49d734c9502216d977
Sunanda Chakraborty <109503742+sunanda073@users.noreply.github.com>
2023-09-22 12:39:29 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,37 @@
+// let score = undefined
+
+// console.log(typeof score)
+// console.log(typeof(score))
+// console.log(score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber); 
+
+/*
+"33" => 33
+"33abc" => NaN (Not an Number) (type => number)
+true => 1 and false => 0
+null => 0
+undefined => NaN
+String (sunanda) => NaN
+*/
+
+// let isLoggedIn = " "
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+// console.log(booleanIsLoggedIn);
+
+/*
+""(empty string) => false
+string ("sunanda") => true
+1 => true
+0 =>false
+*/
+
+let someNumber = 33.30
+let stringNumber = String(someNumber)
+
+console.log(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
